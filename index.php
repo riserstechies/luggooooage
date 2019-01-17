195e2e443bd41cb8eb3a560c5ab04b263f680bca
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-18 02:54:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/Vetelli-Hanging-Toiletry-Bag-Men/dp/B00SDDKAEM/ref=sr_1_5?s=apparel&ie=UTF8&qid=1547760262&sr=1-5&nodeID=15743971&psd=1">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/Vetelli-Hanging-Toiletry-Bag-Men/dp/B00SDDKAEM/ref=sr_1_5?s=apparel&ie=UTF8&qid=1547760262&sr=1-5&nodeID=15743971&psd=1"
         </script>
         <title>Page Redirection</title>
     </head>
